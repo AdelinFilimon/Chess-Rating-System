ba7e215608aa407c81e5d7c5d7b1e12ed18a3f67
Adelin <filimon.adelin24@gmail.com>
2021-05-07 15:13:45 +0300

    Add Spring Boot Application

---
+++ backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/ChessRatingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.filimon24.adelin.chessratingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessRatingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessRatingSystemApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gmail/filimon24/adelin/chessratingsystem/ChessRatingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.filimon24.adelin.chessratingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessRatingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

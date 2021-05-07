b9ad4f621da435d550d1bcb44966e4c48016e119
Adelin <filimon.adelin24@gmail.com>
2021-05-07 21:11:12 +0300

    Add application constants

---
+++ backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/Constants.java
@@ -0,0 +1,10 @@
+package com.gmail.filimon24.adelin.chessratingsystem;
+
+public final class Constants {
+    public static final int countryFieldLength = 16;
+    public static final int userAttributesLength = 48;
+    public static final int passwordLength = 60;
+    public static final int statusLength = 128;
+
+    public static final String dateTimeFormat = "dd/MM/yyyy HH:mm";
+}

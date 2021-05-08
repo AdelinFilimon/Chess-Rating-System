6748ab9b539f4e46a3ecaa89094029eb25364287
Adelin <filimon.adelin24@gmail.com>
2021-05-08 16:59:36 +0300

    Add CountryConverter class for Country enum conversion between database and entity attribute

---
+++ backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/persistence/util/CountryConverter.java
@@ -0,0 +1,20 @@
+package com.gmail.filimon24.adelin.chessratingsystem.persistence.util;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class CountryConverter implements AttributeConverter<Country, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Country country) {
+        if (country == null) return null;
+        return country.getCode();
+    }
+
+    @Override
+    public Country convertToEntityAttribute(String countryCode) {
+        if (countryCode == null) return null;
+        return Country.valueOf(countryCode);
+    }
+}

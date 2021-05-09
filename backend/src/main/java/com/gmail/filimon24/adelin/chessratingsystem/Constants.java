2e02cf46ee27ae9261ca266a9a40d052321b0b21
Adelin <filimon.adelin24@gmail.com>
2021-05-09 12:38:24 +0300

    Role Identifiers Added (Player, Administrator)

--- backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/Constants.java
+++ backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/Constants.java
@@ -1,10 +1,12 @@
 package com.gmail.filimon24.adelin.chessratingsystem;
 
 public final class Constants {
-    public static final int COUNTRY_FIELD_LENGTH = 8;
+    public static final int COUNTRY_FIELD_LENGTH = 3;
     public static final int USER_ATTRIBUTE_LENGTH = 48;
     public static final int USER_PASSWORD_LENGTH = 60;
     public static final int USER_STATUS_LENGTH = 128;
 
     public static final String DATETIME_FORMAT = "dd/MM/yyyy HH:mm";
+    public static final String PLAYER_ROLE_IDENTIFIER = "ROLE_PLAYER";
+    public static final String ADMINISTRATOR_ROLE_IDENTIFIER = "ROLE_ADMINISTRATOR";
 }

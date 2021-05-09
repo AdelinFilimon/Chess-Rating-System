87818beb99e4168a574fd2c97e1fc5b4764bc77f
Adelin <filimon.adelin24@gmail.com>
2021-05-09 12:39:04 +0300

    Add GameHistoryRepository

---
+++ backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/persistence/GameHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.gmail.filimon24.adelin.chessratingsystem.persistence;
+
+import com.gmail.filimon24.adelin.chessratingsystem.persistence.entity.GameHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameHistoryRepository extends JpaRepository<GameHistory, Long> {
+}

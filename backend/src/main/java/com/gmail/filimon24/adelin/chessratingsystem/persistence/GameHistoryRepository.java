8d75eb5e88ecc6b558fcb902f8928b9e67500247
Adelin <filimon.adelin24@gmail.com>
2021-05-14 20:06:49 +0300

    Update after class renames

--- backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/persistence/GameHistoryRepository.java
+++ backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/persistence/GameHistoryRepository.java
@@ -1,9 +1,9 @@
 package com.gmail.filimon24.adelin.chessratingsystem.persistence;
 
-import com.gmail.filimon24.adelin.chessratingsystem.persistence.entity.GameHistory;
+import com.gmail.filimon24.adelin.chessratingsystem.persistence.entity.GameHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameHistoryRepository extends JpaRepository<GameHistory, Long> {
+public interface GameHistoryRepository extends JpaRepository<GameHistoryEntity, Long> {
 }

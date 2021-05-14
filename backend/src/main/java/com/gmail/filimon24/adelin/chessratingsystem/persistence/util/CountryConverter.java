64aae5063e2b9bbde63da000067eeff3c9734a99
Adelin <filimon.adelin24@gmail.com>
2021-05-14 20:04:46 +0300

    Solved bug on convertToEntityAttribute method

--- backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/persistence/util/CountryConverter.java
+++ backend/src/main/java/com/gmail/filimon24/adelin/chessratingsystem/persistence/util/CountryConverter.java
@@ -2,6 +2,8 @@ package com.gmail.filimon24.adelin.chessratingsystem.persistence.util;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Converter(autoApply = true)
 public class CountryConverter implements AttributeConverter<Country, String> {
@@ -15,6 +17,9 @@ public class CountryConverter implements AttributeConverter<Country, String> {
     @Override
     public Country convertToEntityAttribute(String countryCode) {
         if (countryCode == null) return null;
-        return Country.valueOf(countryCode);
+        return Arrays.stream(Country.values())
+                .filter(country -> country.getCode().equals(countryCode))
+                .collect(Collectors.toList())
+                .get(0);
     }
 }
